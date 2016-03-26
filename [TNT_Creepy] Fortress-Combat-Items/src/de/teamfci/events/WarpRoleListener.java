6aa0d6765efa418d7a738c32374b2a585439d244
FilipZocktan <filipzocktan@gmail.com>
2016-03-26 22:53:40 +0100

    Added WarpRoleListener

---
+++ [TNT_Creepy] Fortress-Combat-Items/src/de/teamfci/events/WarpRoleListener.java
@@ -0,0 +1,32 @@
+package de.teamfci.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class WarpRoleListener {
+	@EventHandler
+	public void onInteract(PlayerInteractEvent ev){
+		Action ac = ev.getAction();
+		if(ac.equals(Action.RIGHT_CLICK_AIR)){
+			Player p = ev.getPlayer();
+			if(p.getItemInHand().getItemMeta().getLore().get(1).contains("Rechtsklick um zum Spawn zu gelangen")){
+				p.performCommand("spawn");
+				ItemStack newroles = p.getItemInHand();
+				newroles.setAmount(newroles.getAmount()-1);
+				p.setItemInHand(newroles);
+			}
+		}
+		if(ac.equals(Action.RIGHT_CLICK_BLOCK)){
+			Player p = ev.getPlayer();
+			if(p.getItemInHand().getItemMeta().getLore().get(1).contains("Rechtsklick um zum Spawn zu gelangen")){
+				p.performCommand("spawn");
+				ItemStack newroles = p.getItemInHand();
+				newroles.setAmount(newroles.getAmount()-1);
+				p.setItemInHand(newroles);
+			}
+		}
+	}
+}
